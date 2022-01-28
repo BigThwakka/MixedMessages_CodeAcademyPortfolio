eaebff3aaf8bcf4ad4609fcec20072ad298ed430
Lloyd <leo10154@hotmail.co.uk>
2022-01-28 21:21:54 +0000

    Added first section of code

---
+++ messageGenerator.js
@@ -0,0 +1,28 @@
+/*
+CodeAcademy Portfolio - Mixed Messages
+ Coded by Lloyd; January 26th 2021
+
+ ---- TASK: ----
+    - Message Generator
+    - Run Program > Outputs Random Message
+    - Comprised of three segments in Output
+
+ ---- REQUIREMENTS: ----
+    - Javascript
+    - Git version Control
+    - Command Line
+
+ ---- TOPIC: 'Butchered' Movie Quotes----
+    - 'No, I am your father!'
+    - 'RAAARGH, Get to the Chopper!'
+    - 'Uh say Jarvis, is it that time?'
+*/
+
+// Define Quotes in Arrays
+const quote1 = ["No,", " I am your ", " father!"]
+const quote2 = ["RAAARGH,", " Get to the ", "Chopper!"]
+const quote3 = ["Uh say Jarvis,", " is it that ", "time?"]
+
+const MessageRandomizer = (quote1, quote2, quote3) => {
+   
+}
